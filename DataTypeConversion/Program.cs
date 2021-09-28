d9d476078bd548912629f19f29fa718ba06eb7c2
Luiz Parente <luiz.par@outlook.com>
2021-09-27 22:15:08 -0400

    Data type conversion and exercises.

---
+++ DataTypeConversion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataTypeConversion {
+	class Program {
+		static void Main(string[] args) {
+			// Everything the user types in the console is a string.
+			string inputPrice = "14.99";
+			string inputQuantity = "3";
+			string inputHasDiscount = "false";
+			string inputCashier = "H";
+
+			// Converting string to double
+			//double price = Convert.ToDouble(input);
+			double price = double.Parse(inputPrice);
+
+			// Converting string to int
+			int quantity = Convert.ToInt32(inputQuantity);
+			//int quantity = int.Parse(inputQuantity);
+
+			// Converting string to bool
+			//bool hasDiscount = Convert.ToBoolean(inputHasDiscount);
+			bool hasDiscount = bool.Parse(inputHasDiscount);
+
+			// Converting string to char
+			char cashier = Convert.ToChar(inputCashier);
+			//char casher = char.Parse(inputCashier);
+
+
+			// Casting is a data type conversion technique that can be used
+			// as long as we are not converting to or from a string
+
+			// Example: Converting 20 degrees Celsius to Fahrenheit
+
+			double celsius = 20;
+			double fahrenheit = (double)9 / 5 * celsius + 32;
+
+			Console.WriteLine(fahrenheit);
+		}
+	}
+}
---
+++ Exercise02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise02 {
+	class Program {
+		static void Main(string[] args) {
+			// Write a console application that prompts the user for:
+			// - Product price
+			// - Quantity
+			// - Tax rate
+
+			// And displays the order total, formatted as a currency value.
+
+			// DECLARE VARIABLES
+			double productPrice;
+			double quantity;
+			double taxRate;
+			double orderTotal;
+
+			// COLLECTING INPUT DATA
+			Console.WriteLine("Please enter the product price: ");
+			productPrice = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Please enter the quantity: ");
+			quantity = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Please enter the tax rate in decimal format: ");
+			taxRate = double.Parse(Console.ReadLine());
+
+			// ALGORITHM
+			orderTotal = productPrice * quantity * (1 + taxRate);
+
+			// DISPLAY OUTPUT
+			Console.WriteLine($"\nThe order total is {orderTotal:C}");
+
+		}
+	}
+}
