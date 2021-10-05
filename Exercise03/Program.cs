25bd4d1e0c2af5c8a25c9f1c886796e524e53f22
Luiz Parente <luiz.par@outlook.com>
2021-10-04 22:25:40 -0400

    Practice exercises.

---
+++ Exercise03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise03 {
+	class Program {
+		static void Main(string[] args) {
+			// This is where the magic happens.
+
+			/*
+			
+			Write a program to ask the user about the validity of a simple statement. 
+			
+			The program should accept the response then display the statement as well as the response. 
+			The response should be true or false. For this question, you must use a variable of type bool. 
+			It is useful to know that Convert.ToBoolean() can work with true, True, tRue, TRUE etc.
+
+			*/
+
+			// DECLARE VARIABLES
+			bool response;
+			string statement = "In C#, variables can be declared and initialized at the same time.";
+
+			// COLLECT INPUT
+			Console.WriteLine(statement);
+			Console.WriteLine("True or false?");
+			response = Convert.ToBoolean(Console.ReadLine());
+
+			// ALGORITHM
+
+			// DISPLAY OUTPUT
+			Console.WriteLine("\nThe sentence was:");
+			Console.WriteLine(statement);
+			Console.WriteLine($"\nThe input was: {response}");
+			Console.WriteLine("The correct answer was: true");
+		}
+	}
+}
---
+++ Exercise04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise04 {
+	class Program {
+		static void Main(string[] args) {
+			// This is where the magic happens.
+
+			/*
+			 
+			Write a program that prompts the user for a number (that may be a fraction). 
+			
+			The program reads in the input and print the following: the input as a double, the input as an int and finally the input as a char.
+			e.g. if the input is 65.790, then the output will be 65.790, 65, and A. 
+			 
+			*/
+
+			// DECLARE VARIABLES
+			double numerator;
+			double denominator;
+
+			// COLLECT INPUT
+			Console.WriteLine("Please input the numerator: ");
+			numerator = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Please input the denominator: ");
+			denominator = Convert.ToDouble(Console.ReadLine());
+
+			// ALGORITHM
+
+			// DISPLAY OUTPUT
+			Console.WriteLine($"\nOutput as char: {(char)(numerator / denominator)}");
+			Console.WriteLine($"Output as double: {(numerator / denominator):F3}");
+			Console.WriteLine($"Output as int: {(int)numerator / (int)denominator}");
+		}
+	}
+}
